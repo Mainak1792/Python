4d76c65b16aa44884e501a0b7a00e585f742206f
Mainak Chakraborty <76518189+Mainak1792@users.noreply.github.com>
2021-05-05 21:28:07 +0530

    Update Insertion_sort.py

--- Insertion_sort.py
+++ Insertion_sort.py
@@ -4,4 +4,4 @@ def insertionsort(seq):
     while pos>0 and seq[pos]<seq[pos-1]: #to_Check_whether_the_position_is_lower_than_its_left_element
       (seq[pos],seq[pos-1])=(seq[pos-1],seq[pos]) #swap_and_send_the_element_to_left
       pos=pos-1
-#complexity=O(n)
+#complexity=O(n^2)

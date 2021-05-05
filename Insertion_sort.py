a2d452fd0c12ff4a956b05c75c3a1dccbb512379
Mainak Chakraborty <76518189+Mainak1792@users.noreply.github.com>
2021-05-05 21:27:54 +0530

    Update Insertion_sort.py

--- Insertion_sort.py
+++ Insertion_sort.py
@@ -1,6 +1,7 @@
 def insertionsort(seq):
-  for slice in range(len(seq)):
+  for slice in range(len(seq)): #iterate_the_positions
     pos=slice
-    while pos>0 and seq[pos]<seq[pos-1]:
-      (seq[pos],seq[pos-1])=(seq[pos-1],seq[pos])
+    while pos>0 and seq[pos]<seq[pos-1]: #to_Check_whether_the_position_is_lower_than_its_left_element
+      (seq[pos],seq[pos-1])=(seq[pos-1],seq[pos]) #swap_and_send_the_element_to_left
       pos=pos-1
+#complexity=O(n)

a1601ef80e17835a0f210617dea160f2528b93e7
Mainak Chakraborty <76518189+Mainak1792@users.noreply.github.com>
2021-06-05 10:07:14 +0530

    Update Binary_Search.py

--- Binary_Search.py
+++ Binary_Search.py
@@ -10,3 +10,35 @@ def bsearch(seq,v,l,r):
    else:
       return(bsearch(seq,v,mid+l,r)) #iterate_till_mid+1
    
+def binary_search(lst,target):
+    """"
+    Returns index of the element present in the list
+    """
+    first= 0
+    last= len(lst)-1
+    while first<=last:
+        midpoint = (first+last)//2
+        if lst[midpoint]==target:
+            return midpoint
+        elif lst[midpoint]<target:
+            first=midpoint+1
+        else:
+            last=midpoint-1
+    return None
+
+def bsearc(lst,v):
+    if len(lst)==0:
+        return False
+    m= len(lst)//2
+    if lst[m]==v:
+        return True
+    else:
+        if lst[m]<v:
+            return bsearc(lst[m+1:],v)
+        else:
+            return bsearc(lst[:m+1],v)
+
+
+
+numbers=[1,2,3,7,4,56,778]
+print(bsearc(numbers,7))
